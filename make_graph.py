e303b81f98fbe64aeca510f00cc306e3a2f59ca1
Han Lazarus <han.lazarus@gmail.com>
2016-12-22 14:55:15 -0800

    Makes graph of game board without game pieces

--- make_graph.py
+++ make_graph.py
@@ -1,98 +1,114 @@
-def main(board_size=16):
+# Board configuration specific barrier locations are defined here.
+# Indices refer to lines between tiles.
+#
+# Barrier locations are taken from page 3 of DriftingDroids user documentation.
+horizontal_barriers = [
+    (1, 4),
+    (3, 1), (3, 11),
+    (4, 0), (4, 6), (4, 15),
+    (6, 2), 
+    (7, 7), (7, 8), (7, 10), (7, 13),
+    (8, 5), 
+    (9, 7), (9, 8), (9,12),
+    (10, 1), (10, 15),
+    (11, 4), (11, 10),
+    (12, 0),
+    (13, 5),
+    (14, 3), (14, 9), (14, 14),
+    ]
 
-    other_robots = [] 
+vertical_barriers = [
+    (0, 4), (0, 10), 
+    (1, 6), (1, 14),
+    (2, 1), (2, 11),
+    (4, 6),
+    (6, 3), (6, 14),
+    (7, 7), (7, 9), (7, 11),
+    (8, 6), (8, 7), (8, 9),
+    (9, 2), (9, 13),
+    (10, 4), (10, 9),
+    (13, 6), (13, 5),
+    (14, 3), (14, 9),
+    (15, 7), (15, 11)
+    ]
+
+def main(board_size=16, horizontal_barriers=horizontal_barriers, vertical_barriers=vertical_barriers):
+    """ Return a dictionary of game board tiles and associated information for 
+    each tile, including a
+    .
+    Make adjacency list showing edges (lines of travel for game pieces) 
+    between game board tiles
+    """
     board = {}
 
-    #Board configuration specific barrier locations
-    #indices refer to lines between tiles
+    def make_paths(start, destination):
+        """Add path to each tile in list of start tile's to destination where robot can travel."""
+        for tile in start:
+            board[tile]['destinations'].append(destination) 
 
-    #barrier locations taken from page 3 of DriftingDroids user documentation
-    #I'd like to permute barriers/check and check if solvable within certain # of moves
-    #to create other board set ups
+    def make_graph(outer_range, inner_range, barrier_side, direction):
+        """Represent game board as directed graph by adding adjacency list to each tile."""
+        for outer in outer_range: 
+            path = []
+            for inner in inner_range: 
 
-    horizontal_barriers = [
-        (1, 4),
-        (3, 1), (3, 11),
-        (4, 0), (4, 6), (4, 15),
-        (6, 2), 
-        (7, 7),  (7, 8), (7, 10), (7, 13),
-        (8, 5), 
-        (9, 7), (9, 8), (9,12),
-        (10, 1), (10, 15),
-        (11, 4), (11, 10),
-        (12, 0),
-        (13, 5),
-        (14, 9), (14, 14),
-        ]
+                # Set axis of traversal
+                if direction is 'row':
+                    (row, col) = (outer, inner)
+                elif direction is 'col':
+                    (row, col) = (inner, outer)
+                
+                if board[(row, col)]['barriers'][barrier_side] != 1:
+                    path.append((row, col))
+                elif board[(row, col)]['barriers'][barrier_side] == 1:
+                    make_paths(path, (row, col))
+                    path = []
 
-    vertical_barriers = [
-        (0, 4), (0, 10), 
-        (1, 6), (1, 14),
-        (2, 1), (2, 11),
-        (4, 6),
-        (6, 3), (6, 14),
-        (7, 7), (7, 9), (7, 11),
-        (8, 6), (8, 7), (8, 9),
-        (9, 2), (9, 13),
-        (10, 4), (10, 9),
-        (13, 6), (13, 5),
-        (14, 3), (14, 9),
-        (15, 7), (15, 11)
-        ]
-
-    #Horizontal barriers
-    # for i in range(board_size + 1): #row
-    #     for j in range(board_size): #column
-    #         #from board design
-    #         if (i, j) in horizontal_barriers:
-    #             horizontal_barriers[(i, j)] = 1
-            # #from edges of board
-            # else if (i == 0 || i == board_size + 1):
-            #     horizontal_barriers[(i, j)] = 1
-            # #no barrier on other tiles
-            # else:
-            #     horizontal_barriers[(i, j)] = 0
-
-    #Veritical barriers
-    # for i in range(board_size): #row
-    #     for j in range(board_size + 1): #column
-    #         if (i, j) in vertical_barriers:
-    #             vertical_barriers[(i, j)] = 1
-            # else if (j == 0 || j == board_size + 1):
-            #     vertical_barriers[(i,j)] = 1
-            # else:
-            #     vertical_barriers[(i,j)] = 0
-
-
-    for i in range(board_size): # columns
-        for j in range(board_size): # rows
-            board[(i, j)] = {
+    # Incorporate lists of barrier locations and edge barriers into each tile definition.   
+    for row in range(board_size): 
+        for col in range(board_size): 
+            board[(row, col)] = {
             'occupied': 0,
-            'barriers': {'top': 0, 'right': 0, 'bottom': 0, 'left': 0, 'diagonal': 0 }
+            'barriers': {'top': 0, 'right': 0, 'bottom': 0, 'left': 0, 'diagonal': 0 },
+            'destinations': [] 
             }
 
-            # add barriers at all edges of game board
-            if (i == 0):
-                board[(i, j)]['barriers']['top'] = 1
-            if (i == board_size - 1):
-                board[(i, j)]['barriers']['bottom'] = 1
-            if (j == 0):
-                board[(i, j)]['barriers']['left'] = 1
-            if (j == board_size - 1):
-                board[(i, j)]['barriers']['right'] = 1
+            # Add barriers at all edges of game board.
+            if (col == 0):
+                board[(row, col)]['barriers']['left'] = 1
+            if (col == board_size - 1):
+                board[(row, col)]['barriers']['right'] = 1
+            if (row == 0):
+                board[(row, col)]['barriers']['top'] = 1
+            if (row == board_size - 1):
+                board[(row, col)]['barriers']['bottom'] = 1
 
-            # add barriers at board setup specific locations
-            if (i, j) in vertical_barriers:
-                board[(i, j)]['barriers']['left'] = 1
-                board[(i, j - 1)]['barriers']['right'] = 1
+            # Add barriers at board setup specific locations.
+            if (row, col) in vertical_barriers:
+                board[(row, col)]['barriers']['left'] = 1
+                board[(row, col - 1)]['barriers']['right'] = 1
 
-            if (i, j) in horizontal_barriers:
-                board[(i, j)]['barriers']['bottom'] = 1 
-                board[(i - 1, j)]['barriers']['top'] = 1
+            if (row, col) in horizontal_barriers:
+                board[(row, col)]['barriers']['top'] = 1 
+                board[(row - 1, col)]['barriers']['bottom'] = 1
 
+            # Add barriers for any game piece.    
+            #if board[(row, col)]['occupied'] == 1:
 
-    
-    print(board[(5,0)])
+    #Direction of loop to find paths between tiles
+    forward = range(board_size)
+    backward = range(board_size - 1, -1, -1)
+
+    # Traverse rows of board left to right.
+    make_graph(forward, forward, 'right', 'row')
+    # Traverse rows of board right to left.
+    make_graph(forward, backward, 'left', 'row')
+    # Traverse columns of board top to bottom.
+    make_graph(forward, forward, 'bottom', 'col')
+    # Traverse columns of board bottom to top.
+    make_graph(forward, backward, 'top', 'col')
+
+    return board                
 
 if __name__ == '__main__':
     main()
